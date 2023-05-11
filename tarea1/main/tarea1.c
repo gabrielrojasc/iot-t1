28e0a63d104713055c611f9bdbd829b75d4bbf69
Gabriel Rojas <gabo06123@gmail.com>
2023-05-11 19:40:25 -0400

    udp_client copy

--- tarea1/main/tarea1.c
+++ tarea1/main/tarea1.c
@@ -12,6 +12,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include "driver/gpio.h"
+
+#define BUTTON_PIN 21
+
 extern void tcp_client(void);
 
 void app_main(void)
@@ -26,10 +30,17 @@ void app_main(void)
      */
     ESP_ERROR_CHECK(example_connect());
 
+    gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BUTTON_PIN, GPIO_PULLUP_ONLY);
+    int buttonStatePrev = 1;
+    int buttonStateNow = 1;
+
     while (true)
     {
+        // buttonStatePrev = buttonStateNow;
+        // buttonStateNow = gpio_get_level(BUTTON_PIN);
+        // ESP_LOGI("main", "Button state: %d", buttonStateNow);
         tcp_client();
-        sleep(3);
-        ESP_LOGI("main", "Reconnecting");
+        // ESP_LOGI("main", "Reconnecting");
     }
 }
