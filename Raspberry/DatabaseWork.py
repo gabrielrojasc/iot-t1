c39562806f0480f1d10867d6f59a6e8e747e7931
Alfredo Escobar <70735090+alfredo-escobar@users.noreply.github.com>
2023-05-12 14:45:07 -0400

    DatabaseWork comentarios y fix

--- Raspberry/DatabaseWork.py
+++ Raspberry/DatabaseWork.py
@@ -16,11 +16,13 @@ def dataSave(header, data):
         row = cursor.fetchone()
         previous_timestamp = row[0] if row else None
 
+        # Datos comunes para todos los protocolos
         cur.execute('''insert into Datos (id_device, MAC, transport_layer, protocol, length, Val1, Batt_level, Timestamp) values (?, ?, ?, ?, ?, ?, ?, ?)''',
                     (header["id_device"], header["MAC"], header["transport_layer"], header["protocol"], header["length"], header["protocol"], data["Val: 1"], data["Batt_level"], now))
 
         row_id = cur.lastrowid
 
+        # Updates de Datos para protocolos específicos
         if header["protocol"] in [1,2,3,4]:
 
             cur.execute('''UPDATE Datos SET Temp = ?, Press = ?, Hum = ?, Co = ? WHERE id_datos = ?''',
@@ -36,12 +38,19 @@ def dataSave(header, data):
             cur.execute('''UPDATE Datos SET Amp_x = ?, Frec_x = ?, Amp_y = ?, Frec_y = ?, Amp_z = ?, Frec_z = ? WHERE id_datos = ?''',
                 (data["Amp_x"], data["Frec_x"], data["Amp_y"], data["Frec_y"], data["Amp_z"], data["Frec_z"], row_id))
 
-
+        # Insert de Logs
         cur.execute('''insert into Logs (datos, id_device, transport_layer, protocol, Timestamp) values (?, ?, ?, ?, ?)''',
                     (row_id, header["id_device"], header["transport_layer"], header["protocol"], now))
 
+        # Insert de Configuracion
         cur.execute('''insert into Configuracion (protocol, transport_layer) values (?, ?)''',
                     (header["protocol"], header["transport_layer"])) # No sé realmente si así haya que trabajar con esta tabla
 
+        # Insert de Loss
+        if previous_timestamp is not None:
+            delay = now - previous_timestamp
+        else:
+            delay = None
+
         cur.execute('''insert into Loss (datos, Timedelay, Packet_loss) values (?, ?, ?)''',
-                    (row_id, now - previous_timestamp, 0)) # Cambiar para guardar el Packet_loss
+                    (row_id, delay, 0)) # Cambiar para guardar el Packet_loss
