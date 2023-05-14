385526220586a51137d2b401464dee8390be7395
Gabriel Rojas <gabo06123@gmail.com>
2023-05-14 19:10:51 -0400

    fasda

--- Raspberry/db.py
+++ Raspberry/db.py
@@ -16,6 +16,7 @@ def get_configs():
 def save_packet_loss(row_id, previous_timestamp, bytes_loss):
     now = datetime.datetime.now()
     with sql.connect("DB.sqlite") as con:
+        cur = con.cursor()
         if previous_timestamp is not None:
             delay = now - previous_timestamp
         else:
@@ -38,76 +39,75 @@ def data_save(header, data, bytes_loss):
         row = cursor.fetchone()
         previous_timestamp = row[0] if row else None
 
-        if header is None:
-            save_packet_loss(None, previous_timestamp, bytes_loss)
-            return
-
-        # Datos comunes para todos los protocolos
-        cur.execute(
-            """
-            insert into Datos
-                (id_device, MAC, transport_layer, protocol, length, Val1, Batt_level)
-                values (?, ?, ?, ?, ?, ?, ?)
-            """,
-            (
-                header["id_device"],
-                header["MAC"],
-                header["transport_layer"],
-                header["protocol"],
-                header["length"],
-                data["Val: 1"],
-                data["Batt_level"],
-            ),
-        )
-
-        row_id = cur.lastrowid
-
-        # Updates de Datos para protocolos específicos
-        if header["protocol"] in [1, 2, 3, 4]:
+        row_id = None
+        if header is not None:
+            # Datos comunes para todos los protocolos
             cur.execute(
-                """UPDATE Datos SET Temp = ?, Press = ?, Hum = ?, Co = ? WHERE id_datos = ?""",
-                (data["Temp"], data["Press"], data["Hum"], data["Co"], row_id),
-            )
-
-        if header["protocol"] in [2, 3]:
-            cur.execute(
-                """UPDATE Datos SET RMS = ? WHERE id_datos = ?""", (data["RMS"], row_id)
-            )
-
-        if header["protocol"] in [3]:
-            cur.execute(
-                """UPDATE Datos SET Amp_x = ?, Frec_x = ?, Amp_y = ?, Frec_y = ?, Amp_z = ?, Frec_z = ? WHERE id_datos = ?""",
+                """
+                insert into Datos
+                    (id_device, MAC, transport_layer, protocol, length, Val1, Batt_level)
+                    values (?, ?, ?, ?, ?, ?, ?)
+                """,
                 (
-                    data["Amp_x"],
-                    data["Frec_x"],
-                    data["Amp_y"],
-                    data["Frec_y"],
-                    data["Amp_z"],
-                    data["Frec_z"],
-                    row_id,
+                    header["id_device"],
+                    header["MAC"],
+                    header["transport_layer"],
+                    header["protocol"],
+                    header["length"],
+                    data["Val: 1"],
+                    data["Batt_level"],
                 ),
             )
 
-        if header["protocol"] in [4]:
+            row_id = cur.lastrowid
+
+            # Updates de Datos para protocolos específicos
+            if header["protocol"] in [1, 2, 3, 4]:
+                cur.execute(
+                    """UPDATE Datos SET Temp = ?, Press = ?, Hum = ?, Co = ? WHERE id_datos = ?""",
+                    (data["Temp"], data["Press"], data["Hum"], data["Co"], row_id),
+                )
+
+            if header["protocol"] in [2, 3]:
+                cur.execute(
+                    """UPDATE Datos SET RMS = ? WHERE id_datos = ?""",
+                    (data["RMS"], row_id),
+                )
+
+            if header["protocol"] in [3]:
+                cur.execute(
+                    """UPDATE Datos SET Amp_x = ?, Frec_x = ?, Amp_y = ?, Frec_y = ?, Amp_z = ?, Frec_z = ? WHERE id_datos = ?""",
+                    (
+                        data["Amp_x"],
+                        data["Frec_x"],
+                        data["Amp_y"],
+                        data["Frec_y"],
+                        data["Amp_z"],
+                        data["Frec_z"],
+                        row_id,
+                    ),
+                )
+
+            if header["protocol"] in [4]:
+                cur.execute(
+                    """UPDATE Datos SET Acc_x = ?, Acc_y = ?, Acc_z = ? WHERE id_datos = ?""",
+                    (
+                        json.dumps(data["Acc_x"]),
+                        json.dumps(data["Acc_y"]),
+                        json.dumps(data["Acc_z"]),
+                        row_id,
+                    ),
+                )
+
+            # Insert de Logs
             cur.execute(
-                """UPDATE Datos SET Acc_x = ?, Acc_y = ?, Acc_z = ? WHERE id_datos = ?""",
+                """insert into Logs (datos, id_device, transport_layer, protocol) values (?, ?, ?, ?)""",
                 (
-                    json.dumps(data["Acc_x"]),
-                    json.dumps(data["Acc_y"]),
-                    json.dumps(data["Acc_z"]),
                     row_id,
+                    header["id_device"],
+                    header["transport_layer"],
+                    header["protocol"],
                 ),
             )
-
-        # Insert de Logs
-        cur.execute(
-            """insert into Logs (datos, id_device, transport_layer, protocol) values (?, ?, ?, ?)""",
-            (
-                row_id,
-                header["id_device"],
-                header["transport_layer"],
-                header["protocol"],
-            ),
-        )
 
     save_packet_loss(row_id, previous_timestamp, bytes_loss)
